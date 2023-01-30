f6fa991339474a3a61da0c4fc9bde9e742e6dcd0
Utku Sarioglu <utkusarioglu@hotmail.com>
2023-01-30 10:59:18 +0000

    USE NEXT APPDIR

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   reactStrictMode: true,
   distDir: "dist",
+  experimental: {
+    appDir: true,
+  },
   publicRuntimeConfig: {
     domainName: process.env.NEXT_PUBLIC_DOMAIN_NAME,
     scheme: process.env.SCHEME,
